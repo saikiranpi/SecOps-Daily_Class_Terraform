e5cab4e9f4041f90af414a285cd844e98246c5bc
saikiranpi <pinapathruni.saikiran@gmail.com>
2024-10-12 13:30:39 +0530

    changed main file

--- main.tf
+++ main.tf
@@ -7,9 +7,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "workspacesbucket01"
-    key    = "workspace.statefile"
-    region = "us-east-1"
+    bucket         = "workspacesbucket01"
+    key            = "workspace.statefile"
+    region         = "us-east-1"
+    dynamodb_table = "dynamodb-state-locking"
   }
 }
 
